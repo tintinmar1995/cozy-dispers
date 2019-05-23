646aa993e05d6a5b1dfaeeb69f97ebbfa0464f06
tintinmar1995 <mart.77.ma@gmail.com>
2019-05-23 12:35:23 +0200

    Delete test sharing

--- tests/sharing/console.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require_relative 'boot'
-
-at_exit { Helpers.cleanup }
-Helpers.scenario "interactive"
-Helpers.start_mailhog
-
-Pry.start binding, prompt: Pry::SIMPLE_PROMPT, quiet: true
