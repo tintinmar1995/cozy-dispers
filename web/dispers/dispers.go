27ff65536a5daaf55b32ad27f3b36a82f61ed978
tintinmar1995 <martin.masson@protonmail.com>
2019-05-29 08:02:35 +0200

    Refactor Concept Indexor's routes

--- web/dispers/dispers.go
+++ web/dispers/dispers.go
@@ -14,9 +14,17 @@ CONCEPT INDEXOR'S ROUTES : those functions are used on route ./dispers/conceptin
 *
 */
 
-func hashConcept(c echo.Context) error {
-	concept := c.Param("concept")
+func createConcept(c echo.Context) error {
+
+	// TODO: Get concept from body
+
+	// TODO: Decrypt concepts
+
+	// TODO: Create array of hashes
+
+	// TODO: Call HashMeThat for each concept
 	hash, err := enclave.HashMeThat(concept)
+
 	return c.JSON(http.StatusCreated, echo.Map{
 		"ok":   err == nil,
 		"hash": hash,
@@ -34,7 +42,7 @@ func deleteConcept(c echo.Context) error {
 // Routes sets the routing for the dispers service
 func Routes(router *echo.Group) {
 	// TODO : Create a route to retrieve public key
-	router.POST("/conceptindexor/hash/concept=:concept", hashConcept)     // hash a concept (and save the salt if needed)
-	router.DELETE("/conceptindexor/hash/concept=:concept", deleteConcept) // delete a salt in the database
+	router.POST("/conceptindexor/concept", createConcept)   // hash a concept (and save the salt if needed)
+	router.DELETE("/conceptindexor/concept", deleteConcept) // delete a salt in the database
 
 }
