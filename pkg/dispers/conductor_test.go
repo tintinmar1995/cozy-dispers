5988a711c00fd05a5bf1d6f931f83fcd5e8baf83
tintinmar1995 <martin.masson@protonmail.com>
2019-07-11 11:51:22 +0200

    Add tests for subscription process

---
+++ pkg/dispers/conductor_test.go
@@ -0,0 +1,112 @@
+package enclave
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/cozy/cozy-stack/pkg/dispers/network"
+	"github.com/cozy/cozy-stack/pkg/dispers/query"
+	"github.com/cozy/cozy-stack/pkg/dispers/subscribe"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateSubscribeDoc(t *testing.T) {
+
+	// Create the three concepts
+	inputCI := query.InputCI{
+		Concepts: []query.Concept{
+			query.Concept{
+				IsEncrypted: false,
+				Concept:     "julien"},
+			query.Concept{
+				IsEncrypted: false,
+				Concept:     "francois"},
+			query.Concept{
+				IsEncrypted: false,
+				Concept:     "paul"},
+		},
+	}
+
+	err := CreateConceptInConductorDB(&inputCI)
+	assert.NoError(t, err)
+	ci := network.NewExternalActor("conceptindexor")
+	ci.MakeRequest("GET", "concept/julien/false", "application/json", nil)
+	var outputCI query.OutputCI
+	err = json.Unmarshal(ci.Out, &outputCI)
+	assert.NoError(t, err)
+
+	_, err = RetrieveSubscribeDoc(outputCI.Hashes[0].Hash)
+	assert.NoError(t, err)
+
+}
+
+func TestSubscribe(t *testing.T) {
+
+	inputCI := query.InputCI{
+		Concepts: []query.Concept{
+			query.Concept{
+				IsEncrypted: false,
+				Concept:     "aime les fraises"},
+			query.Concept{
+				IsEncrypted: false,
+				Concept:     "aime les framboises"},
+			query.Concept{
+				IsEncrypted: false,
+				Concept:     "joue de la guitare"},
+			query.Concept{
+				IsEncrypted: false,
+				Concept:     "est designer chez cozy"},
+		},
+	}
+
+	err := CreateConceptInConductorDB(&inputCI)
+	assert.NoError(t, err)
+	ci := network.NewExternalActor("conceptindexor")
+	err = ci.MakeRequest("GET", "concept/aime les fraises/false", "application/json", nil)
+	assert.NoError(t, err)
+	var outputCI query.OutputCI
+	err = json.Unmarshal(ci.Out, &outputCI)
+	assert.NoError(t, err)
+
+	var listOfInstance []string
+	docs, err := RetrieveSubscribeDoc(outputCI.Hashes[0].Hash)
+	assert.NoError(t, err)
+	json.Unmarshal(docs[0].EncryptedInstances, &listOfInstance)
+	sizeIni := len(listOfInstance)
+
+	// Make few instances subscribe to Cozy-DISPERS
+	inSubs := subscribe.InputConductor{
+		Concepts:          []string{"aime les fraises"},
+		IsEncrypted:       false,
+		EncryptedInstance: []byte("{\"domain\":\"joel.mycozy.cloud\"}"),
+	}
+	err = Subscribe(&inSubs)
+	assert.NoError(t, err)
+	docs, _ = RetrieveSubscribeDoc(outputCI.Hashes[0].Hash)
+	json.Unmarshal(docs[0].EncryptedInstances, &listOfInstance)
+	size := len(listOfInstance)
+	assert.Equal(t, sizeIni+1, size)
+	inSubs = subscribe.InputConductor{
+		Concepts:          []string{"aime les fraises", "aime les framboises"},
+		IsEncrypted:       false,
+		EncryptedInstance: []byte("{\"domain\":\"paul.mycozy.cloud\"}"),
+	}
+	err = Subscribe(&inSubs)
+	assert.NoError(t, err)
+	docs, _ = RetrieveSubscribeDoc(outputCI.Hashes[0].Hash)
+	json.Unmarshal(docs[0].EncryptedInstances, &listOfInstance)
+	size = len(listOfInstance)
+	assert.Equal(t, sizeIni+2, size)
+	inSubs = subscribe.InputConductor{
+		Concepts:          []string{"aime les fraises", "aime les framboises", "est designer chez cozy"},
+		IsEncrypted:       false,
+		EncryptedInstance: []byte("{\"domain\":\"francois.mycozy.cloud\"}"),
+	}
+	err = Subscribe(&inSubs)
+	assert.NoError(t, err)
+	docs, _ = RetrieveSubscribeDoc(outputCI.Hashes[0].Hash)
+	json.Unmarshal(docs[0].EncryptedInstances, &listOfInstance)
+	size = len(listOfInstance)
+	assert.Equal(t, sizeIni+3, size)
+
+}
--- pkg/dispers/dispers_test.go
+++ pkg/dispers/dispers_test.go
@@ -2,39 +2,50 @@ package enclave
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 	"testing"
 
-	"github.com/cozy/checkup"
 	"github.com/cozy/cozy-stack/pkg/config/config"
 	"github.com/cozy/cozy-stack/pkg/couchdb"
+	"github.com/cozy/cozy-stack/pkg/dispers/network"
 	"github.com/cozy/cozy-stack/pkg/prefixer"
+	"github.com/cozy/cozy-stack/tests/testutils"
 )
+
+var dispersURL = url.URL{Host: "localhost:8118", Scheme: "http"}
 
 func TestMain(m *testing.M) {
 	config.UseTestFile()
 
-	// First we make sure couchdb is started
-	db, err := checkup.HTTPChecker{URL: config.CouchURL().String()}.Check()
-	if err != nil || db.Status() != checkup.Healthy {
-		fmt.Println("This test need couchdb to run.")
-		os.Exit(1)
-	}
-
+	// Check is CouchDB is running
+	testutils.NeedCouchdb()
+	// Run tests over TestDB
+	prefixerC = prefixer.TestConductorPrefixer
 	prefixerCI = prefixer.TestConceptIndexorPrefixer
 
-	err = couchdb.ResetDB(prefixerCI, "io.cozy.hashconcept")
+	// Reinitiate DB
+	err := couchdb.ResetDB(prefixerCI, "io.cozy.hashconcept")
 	if err != nil {
 		fmt.Printf("Cant reset db (%s, %s) %s\n", prefixerCI, "io.cozy.hashconcept", err.Error())
 		os.Exit(1)
 	}
-
-	if err := couchdb.InitGlobalDB(); err != nil {
-		fmt.Println("Cant init GlobalDB")
+	err = couchdb.ResetDB(prefixerC, "io.cozy.ml")
+	if err != nil {
+		fmt.Printf("Cant reset db (%s, %s) %s\n", prefixerCI, "io.cozy.ml", err.Error())
 		os.Exit(1)
 	}
+	err = couchdb.ResetDB(prefixerC, "io.cozy.instances")
+	if err != nil {
+		fmt.Printf("Cant reset db (%s, %s) %s\n", prefixerCI, "io.cozy.instances", err.Error())
+		os.Exit(1)
+	}
+	couchdb.InitGlobalDB()
+
+	// Hosts is used for conductor_test
+	hosts = []url.URL{dispersURL}
+	network.Hosts = hosts
 
 	res := m.Run()
 	os.Exit(res)
-
 }
