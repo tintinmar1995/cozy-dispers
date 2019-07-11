aa99f13d0d6183edc8a75650374b6e088cef4f70
tintinmar1995 <martin.masson@protonmail.com>
2019-07-11 12:10:41 +0200

    Build Conductor's treatment for queries

--- pkg/dispers/conductor.go
+++ pkg/dispers/conductor.go
@@ -26,7 +26,269 @@ var (
 	prefixerC = prefixer.ConductorPrefixer
 )
 
-// Retrievesubscribe is used to get a Subscribe doc from the Conductor's database.
+// GetTrainingState can be called by the querier to have some information about
+// the process. GetTrainingState retrieves information from the Conductor's database
+// It retreives both the query.QueryDoc and the Metadata
+func GetTrainingState(id string) ([]query.MetadataDoc, query.QueryDoc, error) {
+	fetched := &query.QueryDoc{}
+	err := couchdb.GetDoc(prefixer.ConductorPrefixer, "io.cozy.ml", id, fetched)
+	if err != nil {
+		return nil, *fetched, err
+	}
+
+	metas, err := query.RetrieveMetadata(id)
+	if err != nil {
+		return nil, *fetched, err
+	}
+
+	return metas, *fetched, nil
+}
+
+// Conductor collects every actors playing a part to the query
+type Conductor struct {
+	Query           query.QueryDoc
+	Conceptindexors network.ExternalActor
+	Targetfinders   network.ExternalActor
+	Targets         network.ExternalActor
+	DataAggregators [][]network.ExternalActor
+}
+
+// NewConductor returns a Conductor object to lead the request
+func NewConductor(in query.OutputQ) (*Conductor, error) {
+
+	if in.NumberActors == nil {
+		return &Conductor{}, errors.New("Number of network.ExternalActors should be defined")
+	}
+
+	da := make([][]network.ExternalActor, len(in.SizeAggrLayers))
+	for i, v := range in.SizeAggrLayers {
+		da[i] = network.NewSliceOfExternalActors("dataaggregator", v)
+	}
+
+	// Creating the query.QueryDoc that will be saved in the Conductor's database
+	queryDoc := query.QueryDoc{
+		CheckPoints:                 make([]bool, 6),
+		Concepts:                    in.Concepts,
+		DomainQuerier:               in.DomainQuerier,
+		IsEncrypted:                 in.IsEncrypted,
+		Jobs:                        in.Jobs,
+		LocalQuery:                  in.LocalQuery,
+		NumberActors:                in.NumberActors,
+		PseudoConcepts:              in.PseudoConcepts,
+		SizeAggrLayers:              in.SizeAggrLayers,
+		TargetProfile:               in.TargetProfile,
+		EncryptedAggregateFunctions: in.EncryptedAggregateFunctions,
+		EncryptedConcepts:           in.EncryptedConcepts,
+		EncryptedLocalQuery:         in.EncryptedLocalQuery,
+		EncryptedTargetProfile:      in.EncryptedTargetProfile,
+	}
+	if err := couchdb.CreateDoc(prefixer.ConductorPrefixer, &queryDoc); err != nil {
+		return &Conductor{}, err
+	}
+
+	retour := &Conductor{
+		Query:           queryDoc,
+		Conceptindexors: network.NewExternalActor("conceptindexor"),
+		Targetfinders:   network.NewExternalActor("targetfinder"),
+		Targets:         network.NewExternalActor("target"),
+		DataAggregators: da,
+	}
+
+	return retour, nil
+}
+
+// decryptConcept returns a list of hashed concepts from a list of encrypted concepts
+func (c *Conductor) decryptConcept() error {
+
+	job := "concept/"
+	listOfConcepts := []string{}
+	for _, concept := range c.Query.Concepts {
+		if concept.IsEncrypted {
+			listOfConcepts = append(listOfConcepts, string(concept.EncryptedConcept))
+		} else {
+			listOfConcepts = append(listOfConcepts, concept.Concept)
+		}
+	}
+	job = job + strings.Join(listOfConcepts, "-")
+	if c.Query.IsEncrypted {
+		job = job + "/true"
+	} else {
+		job = job + "/false"
+	}
+
+	c.Conceptindexors.MakeRequest("GET", job, "", nil)
+	c.Conceptindexors.OutMeta.Push(c.Query.QueryID)
+
+	var outputCI query.OutputCI
+	json.Unmarshal(c.Conceptindexors.Out, &outputCI)
+	c.Query.CheckPoints[0] = true
+	c.Query.Concepts = outputCI.Hashes
+	return couchdb.UpdateDoc(prefixer.ConductorPrefixer, &c.Query)
+}
+
+func (c *Conductor) fetchListsOfInstancesFromDB() error {
+
+	encListsOfA := make(map[string][]byte)
+	listsOfA := make(map[string][]string)
+
+	for _, concept := range c.Query.Concepts {
+
+		s, err := subscribe.RetrieveSubscribeDoc(concept.Hash)
+		if err != nil {
+			return err
+		}
+
+		if c.Query.IsEncrypted {
+			encListsOfA[c.Query.PseudoConcepts[string(concept.EncryptedConcept)]] = s[0].EncryptedInstances
+		} else {
+			listsOfA[c.Query.PseudoConcepts[concept.Concept]] = s[0].Instances
+		}
+
+	}
+
+	if c.Query.IsEncrypted {
+		// TODO : encrypt ListsOfAddresses
+		res, _ := json.Marshal(encListsOfA)
+		c.Query.EncryptedListsOfAddresses = res
+	} else {
+		res, _ := json.Marshal(encListsOfA)
+		var listsOfA map[string][]string
+		json.Unmarshal(res, &listsOfA)
+		c.Query.ListsOfAddresses = listsOfA
+	}
+
+	c.Query.CheckPoints[1] = true
+	return couchdb.UpdateDoc(prefixer.ConductorPrefixer, &c.Query)
+}
+
+func (c *Conductor) selectTargets() error {
+
+	inputTF := query.InputTF{
+		IsEncrypted:               c.Query.IsEncrypted,
+		ListsOfAddresses:          c.Query.ListsOfAddresses,
+		TargetProfile:             c.Query.TargetProfile,
+		EncryptedListsOfAddresses: c.Query.EncryptedListsOfAddresses,
+		EncryptedTargetProfile:    c.Query.EncryptedTargetProfile,
+	}
+
+	marshalledInputTF, err := json.Marshal(inputTF)
+	if err != nil {
+		return err
+	}
+
+	err = c.Targetfinders.MakeRequest("POST", "addresses", "application/json", marshalledInputTF)
+	c.Targetfinders.OutMeta.Push(c.Query.QueryID)
+	if err != nil {
+		return err
+	}
+
+	var outputTF query.OutputTF
+	json.Unmarshal(c.Targetfinders.Out, &outputTF)
+	c.Query.EncryptedTargets = outputTF.EncryptedListOfAddresses
+	c.Query.Targets = outputTF.ListOfAddresses
+	c.Query.CheckPoints[2] = true
+	return couchdb.UpdateDoc(prefixer.ConductorPrefixer, &c.Query)
+}
+
+func (c *Conductor) makeLocalQuery() error {
+
+	inputT := query.InputT{
+		IsEncrypted:         c.Query.IsEncrypted,
+		LocalQuery:          c.Query.LocalQuery,
+		Targets:             c.Query.Targets,
+		EncryptedLocalQuery: c.Query.EncryptedLocalQuery,
+		EncryptedTargets:    c.Query.EncryptedTargets,
+	}
+
+	marshalledInputT, _ := json.Marshal(inputT)
+
+	err := c.Targets.MakeRequest("POST", "gettokens", "application/json", marshalledInputT)
+	c.Targets.OutMeta.Push(c.Query.QueryID)
+	if err != nil {
+		return err
+	}
+	var outputT query.OutputT
+	json.Unmarshal(c.Targets.Out, &outputT)
+	c.Query.Data = outputT.Data
+	c.Query.CheckPoints[3] = true
+	return couchdb.UpdateDoc(prefixer.ConductorPrefixer, &c.Query)
+}
+
+func (c *Conductor) aggregate() error {
+
+	// Distributed data in c.Query.SizeAggrLayers[0] parts
+
+	// for each aggregation layer
+	var inputDA query.InputDA
+	outputDA := make([][]query.OutputDA, len(c.Query.SizeAggrLayers))
+	for indexLayer, sizeLayer := range c.Query.SizeAggrLayers {
+
+		inputDA = query.InputDA{
+			Job:          c.Query.Jobs[indexLayer],
+			IsEncrypted:  c.Query.IsEncrypted,
+			EncryptedJob: c.Query.EncryptedAggregateFunctions[indexLayer],
+		}
+		outputDA[indexLayer] = make([]query.OutputDA, sizeLayer)
+		marshalledInputDA, _ := json.Marshal(inputDA)
+
+		for indexDA, da := range c.DataAggregators[indexLayer] {
+
+			err := c.Targetfinders.MakeRequest("POST", "aggregation", "application/json", marshalledInputDA)
+			c.Targetfinders.OutMeta.Push(c.Query.QueryID)
+			if err != nil {
+				return err
+			}
+
+			var out query.OutputDA
+			json.Unmarshal(da.Out, &out)
+			outputDA[indexLayer][indexDA] = out
+		}
+	}
+
+	c.Query.CheckPoints[5] = true
+	c.Query.OutputsDA = outputDA
+	return couchdb.UpdateDoc(prefixer.ConductorPrefixer, &c.Query)
+}
+
+// Lead is the most general method. This is the only one used in CMD and Web's files. It will use the 5 previous methods to work
+func (c *Conductor) Lead() error {
+
+	if c.Query.CheckPoints[0] != true {
+		if err := c.decryptConcept(); err != nil {
+			return err
+		}
+	}
+
+	if c.Query.CheckPoints[1] != true {
+		if err := c.fetchListsOfInstancesFromDB(); err != nil {
+			return err
+		}
+	}
+
+	if c.Query.CheckPoints[2] != true {
+		if err := c.selectTargets(); err != nil {
+			return err
+		}
+	}
+
+	if c.Query.CheckPoints[3] != true {
+		if err := c.makeLocalQuery(); err != nil {
+			return err
+		}
+	}
+
+	// TODO: Deal with Async Method
+	if c.Query.CheckPoints[4] != true {
+		if err := c.aggregate(); err != nil {
+			return err
+		}
+	}
+
+	// TODO: Notify the querier
+	return couchdb.UpdateDoc(prefixer.ConductorPrefixer, &c.Query)
+}
+
+// RetrieveSubscribeDoc is used to get a Subscribe doc from the Conductor's database.
 // It returns an error if there is more than 1 subscribe doc
 func RetrieveSubscribeDoc(hash []byte) ([]subscribe.SubscribeDoc, error) {
 
