943d2373cc943df08d77922a331a339466faaed8
tintinmar1995 <martin.masson@protonmail.com>
2019-07-18 15:53:24 +0200

    Create a "sum" function

---
+++ pkg/dispers/aggregations/univariate.go
@@ -0,0 +1,50 @@
+package aggregations
+
+import "errors"
+
+// Sum takes in input the data
+// a map speciafying some parameters :
+// - keys : Value on which compute the sum. The specified keys should be one of the keys from Data.
+// If key isn't specified, sum will be computed on every attribute of data
+// - weight : specify a key to compute a weighted sum
+func Sum(data []map[string]interface{}, args map[string]interface{}) (map[string]interface{}, error) {
+
+	var keys []string
+
+	if args["keys"] != nil {
+		// We retrieve keys from args
+		keys = args["keys"].([]string)
+	} else {
+		// We collect the keys for one row, every variable will be summed
+		keys = make([]string, 0, len(data[0]))
+		for k := range data[0] {
+			keys = append(keys, k)
+		}
+	}
+
+	sums := make(map[string]interface{})
+
+	for _, key := range keys {
+		// Initiate sum
+		sumKey := 0.0
+		// We iterate over the data and sum the values if possible
+		for _, row := range data {
+			switch row[key].(type) {
+			case string:
+				return nil, errors.New("Unable to sum strings")
+			default:
+				if args["weight"] != nil {
+					sumKey = sumKey + row[key].(float64)/float64(row[args["weight"].(string)].(int))
+				} else {
+					sumKey = sumKey + row[key].(float64)
+				}
+			}
+		}
+		// Add sumKey to the returned structure
+		sums[key] = sumKey
+	}
+
+	sums["length"] = len(data)
+
+	return sums, nil
+}
