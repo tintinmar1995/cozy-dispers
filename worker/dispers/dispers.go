7f490c78609a450bafb876a802b933b423ade5ca
tintinmar1995 <martin.masson@protonmail.com>
2019-07-30 15:08:09 +0200

    Add a new worker

--- worker/dispers/dispers.go
+++ worker/dispers/dispers.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"runtime"
@@ -20,23 +21,60 @@ func init() {
 		MaxExecCount: 2,
 		WorkerFunc:   WorkerDataAggregator,
 	})
+	job.AddWorker(&job.WorkerConfig{
+		WorkerType:   "resume-query",
+		Concurrency:  1, // Prevent that conductor computes twice the same task
+		MaxExecCount: 2,
+		WorkerFunc:   WorkerResumeQuery,
+	})
+}
+
+func handleError(err error) error {
+	if err != nil {
+		fmt.Println(err.Error())
+		// Send err to conductor
+	}
+	return err
+}
+
+func WorkerResumeQuery(ctx *job.WorkerContext) error {
+
+	in := &query.InputResumeQuery{}
+	if err := ctx.UnmarshalMessage(in); err != nil {
+		return handleError(err)
+	}
+
+	queryDoc, err := enclave.NewQueryFetchingQueryDoc(in.QueryID)
+	if err != nil {
+		return handleError(err)
+	}
+	if err = queryDoc.Lead(); err != nil {
+		return handleError(err)
+	}
+	if err = queryDoc.TryToEndQuery(); err != nil {
+		return handleError(err)
+	}
+
+	return nil
 }
 
 // WorkerDataAggregator is a worker that launch DataAggregator's treatment.
 func WorkerDataAggregator(ctx *job.WorkerContext) error {
 	in := &query.InputDA{}
 	if err := ctx.UnmarshalMessage(in); err != nil {
-		return err
+		return handleError(err)
 	}
 
 	if len(in.Data) == 0 {
-		return errors.New("Worker has to receive Data to compute the aggregation")
+		return handleError(errors.New("Worker has to receive Data to compute the aggregation"))
 	}
 
 	res, err := enclave.AggregateData(*in)
 	if err != nil {
-		return err
+		return handleError(err)
 	}
+
+	// TODO: Use External Actor
 
 	out := query.OutputDA{
 		Results:       res,
@@ -44,30 +82,24 @@ func WorkerDataAggregator(ctx *job.WorkerContext) error {
 		AggregationID: in.AggregationID,
 	}
 	in.ConductorURL.Path = "dispers/query/" + in.QueryID
-	inputPatchQuery := query.InputPatchQuery{
-		IsEncrypted: in.IsEncrypted,
-		Role:        "dataaggregator",
-		OutDA:       out,
-	}
-	marshaledInputPatchQuery, err := json.Marshal(inputPatchQuery)
+	marshaledOutputDA, err := json.Marshal(out)
 	if err != nil {
-		return err
+		return handleError(err)
 	}
-
 	client := http.Client{}
-	request, err := http.NewRequest("PATCH", in.ConductorURL.String(), bytes.NewReader(marshaledInputPatchQuery))
+	request, err := http.NewRequest("PATCH", in.ConductorURL.String(), bytes.NewReader(marshaledOutputDA))
 	if err != nil {
-		return err
+		return handleError(err)
 	}
 	request.Header.Set("Content-Type", "application/json")
 
 	resp, err := client.Do(request)
 	if err != nil {
-		return err
+		return handleError(err)
 	}
 
-	_, err = ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 	// TODO: Check conductor's answer
 
-	return err
+	return handleError(err)
 }
