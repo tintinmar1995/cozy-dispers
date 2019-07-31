b0abda1f705d863f587761519d0351ab60347eda
tintinmar1995 <martin.masson@protonmail.com>
2019-07-31 11:48:02 +0200

    Create a doc for asynchrone tasks

---
+++ pkg/dispers/query/async.go
@@ -0,0 +1,215 @@
+package query
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/cozy/cozy-stack/pkg/couchdb"
+	"github.com/cozy/cozy-stack/pkg/couchdb/mango"
+	"github.com/cozy/cozy-stack/pkg/prefixer"
+)
+
+var PrefixerC = prefixer.ConductorPrefixer
+
+type State int
+
+const (
+	Finished State = iota
+	Waiting
+	Running
+	Failed
+)
+
+type AsyncType int
+
+const (
+	AsyncAggregation AsyncType = iota
+)
+
+type Async struct {
+	AsyncID    string                 `json:"_id,omitempty"`
+	AsyncRev   string                 `json:"_rev,omitempty"`
+	QueryID    string                 `json:"queryid"`
+	IndexLayer int                    `json:"layerid"`
+	IndexDA    int                    `json:"daid"`
+	Type       AsyncType              `json:"type"`
+	StateDA    State                  `json:"state"`
+	Data       map[string]interface{} `json:"data"`
+}
+
+// ID returns the Doc ID
+func (as *Async) ID() string {
+	return as.AsyncID
+}
+
+// Rev returns the doc's version
+func (as *Async) Rev() string {
+	return as.AsyncRev
+}
+
+// DocType returns the DocType
+func (as *Async) DocType() string {
+	return "io.cozy.async"
+}
+
+// Clone copy a brand new version of the doc
+func (as *Async) Clone() couchdb.Doc {
+	cloned := *as
+	return &cloned
+}
+
+// SetID set the ID
+func (as *Async) SetID(id string) {
+	as.AsyncID = id
+}
+
+// SetRev set the version
+func (as *Async) SetRev(rev string) {
+	as.AsyncRev = rev
+}
+
+func NewAsyncTask(queryid string, indexLayer int, indexDA int, asyncType AsyncType) (Async, error) {
+	doc := Async{
+		QueryID:    queryid,
+		IndexLayer: indexLayer,
+		IndexDA:    indexDA,
+		Type:       asyncType,
+		StateDA:    Running,
+	}
+
+	err := couchdb.CreateDoc(PrefixerC, &doc)
+
+	return doc, err
+}
+
+func SetAsyncTaskAsFinished(queryid string, indexLayer int, indexDA int) error {
+
+	var out []Async
+
+	err := couchdb.EnsureDBExist(PrefixerC, "io.cozy.async")
+	if err != nil {
+		return err
+	}
+
+	req := &couchdb.FindRequest{Selector: mango.And(mango.Equal("queryid", queryid), mango.Equal("layerid", indexLayer), mango.Equal("daid", indexDA))}
+	err = couchdb.FindDocs(PrefixerC, "io.cozy.async", req, &out)
+	if err != nil {
+		return err
+	}
+
+	if len(out) == 0 {
+		return errors.New("Async task not found")
+	}
+	if len(out) > 1 {
+		return errors.New(queryid + " " + strconv.Itoa(indexLayer) + " " + strconv.Itoa(indexDA) + " Too many async task for this task")
+	}
+
+	out[0].StateDA = Finished
+	if err := couchdb.UpdateDoc(PrefixerC, &out[0]); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func SetData(queryid string, indexLayer int, indexDA int, data map[string]interface{}) error {
+
+	var out []Async
+
+	err := couchdb.EnsureDBExist(PrefixerC, "io.cozy.async")
+	if err != nil {
+		return err
+	}
+
+	req := &couchdb.FindRequest{Selector: mango.And(mango.Equal("queryid", queryid), mango.Equal("layerid", indexLayer), mango.Equal("daid", indexDA))}
+	err = couchdb.FindDocs(PrefixerC, "io.cozy.async", req, &out)
+	if err != nil {
+		return err
+	}
+
+	if len(out) == 0 {
+		return errors.New("Async task not found")
+	}
+	if len(out) > 1 {
+		return errors.New("Too many async task for this task")
+	}
+
+	out[0].Data = data
+	if err := couchdb.UpdateDoc(PrefixerC, &out[0]); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func FetchAsyncStateLayer(queryid string, indexLayer int, sizeLayer int) (State, error) {
+
+	var out []Async
+	req := &couchdb.FindRequest{Selector: mango.And(mango.Equal("queryid", queryid), mango.Equal("layerid", indexLayer))}
+	if err := couchdb.EnsureDBExist(PrefixerC, "io.cozy.async"); err != nil {
+		return Waiting, err
+	}
+	if err := couchdb.FindDocs(PrefixerC, "io.cozy.async", req, &out); err != nil {
+		return Waiting, err
+	}
+
+	if len(out) == 0 {
+		return Waiting, nil
+	}
+
+	if len(out) < sizeLayer {
+		return Running, nil
+	}
+
+	for indexDA := 0; indexDA < sizeLayer; indexDA++ {
+		if out[indexDA].StateDA != Finished {
+			return Running, nil
+		}
+	}
+
+	return Finished, nil
+}
+
+func FetchAsyncStateDA(queryid string, indexLayer int, indexDA int) (State, error) {
+
+	var out []Async
+	req := &couchdb.FindRequest{Selector: mango.And(mango.Equal("queryid", queryid), mango.Equal("layerid", indexLayer), mango.Equal("daid", indexDA))}
+	if err := couchdb.EnsureDBExist(PrefixerC, "io.cozy.async"); err != nil {
+		return Waiting, err
+	}
+	if err := couchdb.FindDocs(PrefixerC, "io.cozy.async", req, &out); err != nil {
+		return Waiting, err
+	}
+
+	if len(out) == 0 {
+		return Waiting, nil
+	}
+
+	if len(out) > 1 {
+		return Finished, errors.New("Too many async task for this task")
+	}
+
+	return out[0].StateDA, nil
+}
+
+func FetchAsyncData(queryid string, indexLayer int, indexDA int) (map[string]interface{}, error) {
+
+	var out []Async
+
+	err := couchdb.EnsureDBExist(PrefixerC, "io.cozy.async")
+	if err != nil {
+		return nil, err
+	}
+
+	req := &couchdb.FindRequest{Selector: mango.And(mango.Equal("queryid", queryid), mango.Equal("layerid", indexLayer), mango.Equal("daid", indexDA))}
+	err = couchdb.FindDocs(PrefixerC, "io.cozy.async", req, &out)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(out) == 0 {
+		return nil, errors.New("No async doc for this job. Perhaps it's still running.")
+	}
+
+	return out[0].Data, nil
+}
---
+++ pkg/dispers/query/async_test.go
@@ -0,0 +1,55 @@
+package query
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/cozy/cozy-stack/pkg/config/config"
+	"github.com/cozy/cozy-stack/pkg/couchdb"
+	"github.com/cozy/cozy-stack/pkg/prefixer"
+	"github.com/cozy/cozy-stack/tests/testutils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAsync(t *testing.T) {
+
+	state, err := FetchAsyncStateLayer("testquery", 0, 4)
+	assert.NoError(t, err)
+	assert.Equal(t, Waiting, state)
+
+	doc, err := NewAsyncTask("testquery", 0, 0, AsyncAggregation)
+	assert.NoError(t, err)
+
+	state, err = FetchAsyncStateLayer("testquery", 0, 4)
+	assert.NoError(t, err)
+	assert.Equal(t, Running, state)
+
+	doc.Data = map[string]interface{}{"hey": "you"}
+	couchdb.UpdateDoc(PrefixerC, &doc)
+	data, err := FetchAsyncData("testquery", 0, 0)
+	assert.NoError(t, err)
+	assert.Equal(t, map[string]interface{}{"hey": "you"}, data)
+
+}
+
+func TestMain(m *testing.M) {
+	config.UseTestFile()
+
+	// Check is CouchDB is running
+	testutils.NeedCouchdb()
+	// Run tests over TestDB
+	PrefixerC = prefixer.TestConductorPrefixer
+
+	// Reinitiate DB
+	err := couchdb.ResetDB(PrefixerC, "io.cozy.async")
+	if err != nil {
+		fmt.Printf("Cant reset db (%s, %s) %s\n", PrefixerC, "io.cozy.instances", err.Error())
+		os.Exit(1)
+	}
+	couchdb.InitGlobalDB()
+
+	// Hosts is used for conductor_test
+	res := m.Run()
+	os.Exit(res)
+}
